7dcaa011a8ec6de1b596933a745bd2f74448e456
Deniz Yildirim <denizyil@outlook.dk>
2021-11-19 00:35:13 +0100

    Change State to be an enum, and add the 4 initial types

--- BackEnd/CoProject.Infrastructure/Entities/State.cs
+++ BackEnd/CoProject.Infrastructure/Entities/State.cs
@@ -1,7 +1,9 @@
 namespace CoProject.Infrastructure.Entities;
 
-public class State
+public enum State
 {
-    // public StateEnum StateId { get; } //TODO: CREATE STATE ENUM, BUT WHERE?
-    // public string Statex { get; set; }
+    Open,
+    Closed,
+    Hidden,
+    Deleted
 }
