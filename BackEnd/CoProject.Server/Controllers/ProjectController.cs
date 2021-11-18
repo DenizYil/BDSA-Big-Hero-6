6a8492ab6d18cc4414351c7f362448167e3d5333
Day0r <skoledanyal@gmail.com>
2021-11-18 18:21:00 +0100

    v0.1 for the ProjectController

---
+++ BackEnd/CoProject.Server/Controllers/ProjectController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoProject.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProjectController
+{
+    [HttpGet(Name = "GetProjects")]
+    public IEnumerable<Project> GetProjects()
+    {
+        return new Project[] {};
+    }
+    
+    [HttpGet(Name = "GetProject")]
+    public IEnumerable<Project> GetProject(int id)
+    {
+        return new Project[] {};
+    }
+    
+    [HttpPost(Name = "CreateProject")]
+    public Project CreateProject(Project p)
+    {
+        return new Project{};
+    }
+    
+    [HttpPut(Name = "ChangeProject")]
+    public Project ChangeProject(int id, Project p)
+    {
+        return new Project{};
+    }
+    
+    [HttpDelete(Name = "DeleteProject")]
+    public void ChangeProject(int id)
+    {
+        return;
+    }
+}
---
+++ BackEnd/CoProject.Server/Project.cs
@@ -0,0 +1,18 @@
+﻿namespace CoProject.Server;
+
+public class Project
+{
+    public int id { get; set; }
+
+    public string name { get; set; }
+
+    public DateTime created { get; set; }
+    
+    public int supervisorID { get; set; }
+    
+    public int? min { get; set; }
+    
+    public int? max { get; set; }
+    
+    public int stateID { get; set; }
+}
