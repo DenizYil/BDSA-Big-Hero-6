43a35ef019b608e80ff06c470f4c94d8ad4728c4
Cromski <90246462+Cromski@users.noreply.github.com>
2021-11-18 21:31:11 +0100

    Added Entities: projectTag, State, Tag, and Userjoined,. State needs an enum

---
+++ BackEnd/CoProject.Infrastructure/Entities/ProjectTag.cs
@@ -0,0 +1,7 @@
+namespace CoProject.Infrastructure.Entities;
+
+public class ProjectTag
+{
+    public int ProjectId { get; set; }
+    public int TagId { get; set; }
+}
---
+++ BackEnd/CoProject.Infrastructure/Entities/State.cs
@@ -0,0 +1,7 @@
+namespace CoProject.Infrastructure.Entities;
+
+public class State
+{
+    // public StateEnum StateId { get; } //TODO: CREATE STATE ENUM, BUT WHERE?
+    // public string Statex { get; set; }
+}
