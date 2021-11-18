cf04ec19ca0483064e222dadbdac35b1068e4aeb
Deniz Yildirim <denizyil@outlook.dk>
2021-11-18 14:29:48 +0100

    Revert "Regenerated and changed to .NET 5"
    This reverts commit fce1e8d652b4243073626d162e1bed6dde92b3a2.

---
+++ BackEnd/CoProject.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace CoProject.Infrastructure.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ BackEnd/CoProject.Server.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace CoProject.Server.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
