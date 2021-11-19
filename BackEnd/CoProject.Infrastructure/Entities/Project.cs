a1f50e58407adfe256d685c669cc98b5396af995
DenizYil <denizyil@outlook.dk>
2021-11-19 13:21:40 +0100

    Update entities

--- BackEnd/CoProject.Infrastructure/Entities/Project.cs
+++ BackEnd/CoProject.Infrastructure/Entities/Project.cs
@@ -9,7 +9,7 @@ public class Project
     public int SupervisorId { get; set; }
     public int? Min { get; set; }
     public int? Max { get; set; }
-    public IReadOnlyCollection<Tag> Tags { get; set; }
-    public IReadOnlyCollection<User> Users { get; set; }
+    //public IReadOnlyCollection<Tag> Tags { get; set; }
+    //public IReadOnlyCollection<User> Users { get; set; }
     public State State { get; set; }
 }
