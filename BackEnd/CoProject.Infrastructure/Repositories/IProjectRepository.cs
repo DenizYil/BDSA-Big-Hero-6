b846a2d3d5b0dbbaefbeb37fcda2da5ffe4eb2e2
Deniz Yildirim <denizyil@outlook.dk>
2021-11-19 23:23:02 +0100

    Add CRUD methods to IProjectRepository

--- BackEnd/CoProject.Infrastructure/Repositories/IProjectRepository.cs
+++ BackEnd/CoProject.Infrastructure/Repositories/IProjectRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace CoProject.Infrastructure.Repositories;
+﻿using CoProject.Infrastructure.DTOs;
 
-public class IProjectRepository : IRepository
+namespace CoProject.Infrastructure.Repositories;
+
+public interface IProjectRepository : IRepository
 {
-    
+    Task<ProjectDTO?> Read(int id);
+    Task<IEnumerable<ProjectDTO>> ReadAll();
+    Task<(Status, int)> Create(ProjectCreateDTO create);
+    Task<Status> Update(ProjectUpdateDTO update);
+    Task<Status> Delete(int id);
 }
