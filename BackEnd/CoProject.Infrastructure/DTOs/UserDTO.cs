f6b9fb41c2bbe16fbe402c26fab7148ea72f4a9f
Deniz Yildirim <denizyil@outlook.dk>
2021-11-20 18:41:11 +0100

    Add UserDTO, containing their name, username and email

---
+++ BackEnd/CoProject.Infrastructure/DTOs/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace CoProject.Infrastructure.DTOs;
+
+public record UserDTO
+{
+    public string Name { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+}
