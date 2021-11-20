21b1bd9841e771960be859308f1d8b3a2f4a312a
Deniz Yildirim <denizyil@outlook.dk>
2021-11-20 18:41:03 +0100

    Add users to project DTOs

--- BackEnd/CoProject.Infrastructure/DTOs/ProjectDTO.cs
+++ BackEnd/CoProject.Infrastructure/DTOs/ProjectDTO.cs
@@ -2,21 +2,23 @@ using CoProject.Infrastructure.Entities;
 
 namespace CoProject.Infrastructure.DTOs;
 
-public record ProjectCreateDTO
+public record ProjectCreateDTO 
 {
     public string Name { get; init; }
     public string Description { get; init; }
     public int SupervisorId { get; init; }
     public int? Min { get; init; }
     public int? Max { get; init; }
-    public IReadOnlyCollection<string> Tags { get; init; }
+    
     public State State { get; init; }
+    public IReadOnlyCollection<string> Tags { get; init; }
 }
 
 public record ProjectDTO : ProjectCreateDTO
 {
     public int Id { get; init; }
     public DateTime Created { get; init; }
+    public IReadOnlyCollection<UserDTO> Users { get; init; }
 }
 
 // TODO: Tjek om det her er en god idé?
