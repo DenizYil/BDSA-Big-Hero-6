6d237d9341e1c9b6e8a683af39cf16701695fbf8
Deniz Yildirim <denizyil@outlook.dk>
2021-11-21 19:44:06 +0100

    Add initial set up

--- BackEnd/CoProject.Infrastructure/DTOs/UserDTO.cs
+++ BackEnd/CoProject.Infrastructure/DTOs/UserDTO.cs
@@ -2,9 +2,26 @@
 
 #pragma warning disable CS8618
 
+// TODO: Replace this with UserDetailsDTO
 public record UserDTO
 {
     public string Name { get; set; }
     public string UserName { get; set; }
     public string Email { get; set; }
+}
+
+public record UserCreateDTO
+{
+    
+}
+
+public record UserUpdateDTO
+{
+    
+}
+
+public record UserDetailsDTO
+{
+    
+    
 }
---
+++ BackEnd/CoProject.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using CoProject.Infrastructure.DTOs;
+
+namespace CoProject.Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private ICoProjectContext _context;
+
+    public UserRepository(ICoProjectContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<UserDetailsDTO?> Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<UserDetailsDTO>> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<UserDetailsDTO> Create(UserCreateDTO create)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Status> Update(UserUpdateDTO update)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Status> Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
